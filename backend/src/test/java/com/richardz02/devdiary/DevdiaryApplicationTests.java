745e1e1f1fd5c912f1f84025b84f6777242637c0
Richard Zhou <richardzhou163@gmail.com>
2025-11-18 00:29:16 -0500

    set up backend structure

---
+++ backend/src/test/java/com/richardz02/devdiary/DevdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richardz02.devdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
