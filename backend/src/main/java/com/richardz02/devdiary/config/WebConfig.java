7a8dc9466d1d4eeb1a0fdf06f729cc1bdbe25a0f
Richard Zhou <richardzhou163@gmail.com>
2025-11-18 10:06:56 -0500

    add config files

---
+++ backend/src/main/java/com/richardz02/devdiary/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.richardz02.devdiary.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**") // adjust the path if needed
+                .allowedOrigins("http://127.0.0.1:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+                // .exposedHeaders("Authorization");
+    }
+}
