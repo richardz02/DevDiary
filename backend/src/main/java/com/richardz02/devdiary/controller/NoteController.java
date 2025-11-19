7d47d43eeff40d218b7362892932d884898dca0b
Richard Zhou <richardzhou163@gmail.com>
2025-11-18 22:50:37 -0500

    add api endpoints and services for creating a new note, updating an existing note, and deleting a note

--- backend/src/main/java/com/richardz02/devdiary/controller/NoteController.java
+++ backend/src/main/java/com/richardz02/devdiary/controller/NoteController.java
@@ -1,22 +1,32 @@
 package com.richardz02.devdiary.controller;
 
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.richardz02.devdiary.dto.ApiResponse;
-import com.richardz02.devdiary.dto.note.NoteDTO;
+import com.richardz02.devdiary.dto.note.NoteRequestDTO;
+import com.richardz02.devdiary.dto.note.NoteResponseDTO;
 import com.richardz02.devdiary.service.NoteService;
 
 @RestController
 @RequestMapping("/api/v1")
 public class NoteController {
     
+    /* 
+     * Dependency Injection:
+     * - NoteService
+     */
     private final NoteService noteService;
-
     public NoteController(NoteService noteService) {
         this.noteService = noteService;
     }
@@ -24,5 +34,31 @@ public class NoteController {
     @GetMapping("/")     
     public String home() {
         return "Welcome to my Dev Diary";
+    }
+
+    @PostMapping("/notes")
+    public ResponseEntity<ApiResponse<NoteResponseDTO>> addNote(@RequestBody NoteRequestDTO addNoteRequest) {
+        NoteResponseDTO noteResponseDTO = noteService.addNote(addNoteRequest);
+
+        // Construct API response with data type NoteResponseDTO
+        ApiResponse<NoteResponseDTO> response = new ApiResponse<NoteResponseDTO>("Success", noteResponseDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/notes/{noteId}")
+    public ResponseEntity<ApiResponse<NoteResponseDTO>> updateNote(@PathVariable UUID noteId, @RequestBody NoteRequestDTO updateNoteRequest) {
+        NoteResponseDTO noteResponseDTO = noteService.updateNote(noteId, updateNoteRequest);
+
+        // Construct API response with data type NoteResponseDTO
+        ApiResponse<NoteResponseDTO> response = new ApiResponse<NoteResponseDTO>("Success", noteResponseDTO);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+    }
+
+    @DeleteMapping("/notes/{noteId}")
+    public ResponseEntity<ApiResponse<Void>> deleteNote(@PathVariable UUID noteId) {
+        noteService.deleteNote(noteId);
+
+        ApiResponse<Void> response = new ApiResponse<Void>("Delete success", null);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
--- backend/src/main/java/com/richardz02/devdiary/service/NoteService.java
+++ backend/src/main/java/com/richardz02/devdiary/service/NoteService.java
@@ -1,17 +1,63 @@
 package com.richardz02.devdiary.service;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
-import com.richardz02.devdiary.dto.note.NoteDTO;
+import com.richardz02.devdiary.dto.note.NoteRequestDTO;
+import com.richardz02.devdiary.dto.note.NoteResponseDTO;
+import com.richardz02.devdiary.mapper.NoteMapper;
 import com.richardz02.devdiary.model.Note;
 import com.richardz02.devdiary.repository.NoteRepository;
 
 @Service
 public class NoteService {
+    
+    /*
+     * Dependency Injection:
+     * - NoteRepository 
+     * - NoteMapper
+     */
     private final NoteRepository noteRepository; 
-
-    public NoteService(NoteRepository noteRepository) {
+    private final NoteMapper noteMapper;
+    public NoteService(NoteRepository noteRepository, NoteMapper noteMapper) {
         this.noteRepository = noteRepository;
+        this.noteMapper = noteMapper;
     }
 
+    public NoteResponseDTO addNote(NoteRequestDTO noteRequestDTO) {
+        // Construct the Note entity from request DTO
+        Note note = noteMapper.toEntity(noteRequestDTO);
+        
+        // Save note to database
+        noteRepository.save(note);
+
+        // Construct the response DTO from the Note entity and return
+        NoteResponseDTO noteResponseDTO = noteMapper.toResponseDTO(note);
+        return noteResponseDTO;
+    }
+
+    public NoteResponseDTO updateNote(UUID noteId, NoteRequestDTO noteRequestDTO) {
+        // Find the note in the database by note id
+        Note note = noteRepository.findById(noteId).orElseThrow(() -> new IllegalArgumentException());
+
+        // Reset fields 
+        note.setTitle(noteRequestDTO.getTitle());
+        note.setBody(noteRequestDTO.getBody());
+
+        // Save updated note
+        noteRepository.save(note);
+
+        // Construct response DTO and return
+        NoteResponseDTO noteResponseDTO = noteMapper.toResponseDTO(note);
+        return noteResponseDTO;
+    }
+
+    public void deleteNote(UUID noteId) {
+        // Find the note in the database by note id
+        Note note = noteRepository.findById(noteId).orElseThrow(() -> new IllegalArgumentException());
+
+        noteRepository.delete(note);
+    }
 }
