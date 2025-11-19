a875c4116a0493636092e12932ecbefaa7c286b2
Richard Zhou <richardzhou163@gmail.com>
2025-11-18 22:41:06 -0500

    update security config file to allow access to h2 database

--- backend/src/main/java/com/richardz02/devdiary/config/SecurityConfig.java
+++ backend/src/main/java/com/richardz02/devdiary/config/SecurityConfig.java
@@ -1,21 +1,39 @@
 package com.richardz02.devdiary.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
             .csrf(csrf -> csrf.disable())
-            .formLogin(login -> login.disable())
-            .httpBasic(basic -> basic.disable());
-
+            .cors(cors -> cors.configurationSource(request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(List.of("http://127.0.0.1:3000"));
+                config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                config.setAllowedHeaders(List.of("*"));
+                config.setExposedHeaders(List.of("Authorization"));
+                config.setAllowCredentials(true);
+                return config;
+            }))
+            // Disable frame blocking from the same origin
+            // So H2 console doesn't get blocked
+            .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/**").permitAll()
+                .requestMatchers("/h2-console/**").permitAll() // Allow connection to h2 database
+                .anyRequest().authenticated());
+                // .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    
         return http.build();
     }
 }
